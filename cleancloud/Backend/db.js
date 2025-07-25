7c6c64281e16c3f4c6128d38a029eb8e6dd371da
Victor Kasikwa <kasikvictor@gmail.com>
2025-07-25 12:02:32 +0300

    Added PostgreSQL configuration URL

--- cleancloud/Backend/db.js
+++ cleancloud/Backend/db.js
@@ -1,2 +1,9 @@
-const { pool } = require('pg');
+const { Pool } = require('pg');
 require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+})
+
+
+module.exports = pool;
