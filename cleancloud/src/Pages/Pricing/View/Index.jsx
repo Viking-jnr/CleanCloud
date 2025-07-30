a10e76c1a6c287ca2f403c3150177f5b0be40baf
Victor Kasikwa <kasikvictor@gmail.com>
2025-07-30 04:03:01 -0700

    Edited the pricing page

--- cleancloud/src/Pages/Pricing/View/Index.jsx
+++ cleancloud/src/Pages/Pricing/View/Index.jsx
@@ -1,5 +1,8 @@
 import { CheckCircle } from "@mui/icons-material";
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import customBrand from "../../../assets/Images/custom-brand.svg"
+import reviewmodule from "../../../assets/Images/review-module.svg"
+import plantmodule from "../../../assets/Images/plant-module.svg"
 
 const plans = [
   {
@@ -60,6 +63,18 @@ const plans = [
   }
 ];
 
+const FAQs = [
+  {faq: "Are there any hidden fees?",
+   answer: "Apart from selecting your subscription plan and setup plan there are no mandatory fees with the system. There are optional additional modules and services, such as the custom branded app that carry an additional fee. Get in touch with us to discuss further."},
+   {faq: "Does the service include an app for me?", 
+    answer: "We include a free booking tool for your website and your store will be on the CleanCloud iOS and Android apps with your branding, which has over 140,000 downloads on iOS and Android. Once customers have selected your store in the app it will remember your store as their default. If you prefer we can build a dedicated branded app for you as an additional service or as part of the Grow+ subscription, please contact us for more information."
+   },
+   {faq: "What if I have more than 3,000 orders per month?", answer: "Contact us with your expected order volume per store and we will be able to provide you with your own dedicated pricing."},
+   {faq: "How long does it take to be fully operational?", answer: "We can set you up overnight with our Express Onboarding, however, we generally recommend 2-6 weeks for a full thorough setup including order monitoring."},
+   {faq: "Are there any contracts and can we change plans?", answer: "We don't do contracts for subscription fees unless there are exceptional circumstances. You can upgrade plans and change from monthly to annual at any time."},
+   {faq: "What are your support hours?", answer: "We offer 24/7 support, our preferred support method is via ticket from within the system. However, we also offer phone support 24/7 for our Pro customers and above."},
+]
+
 const Pricing = () => {
     return(
         <>
@@ -97,7 +112,10 @@ const Pricing = () => {
         }}>
           <h2 style={{ color: '#333' }}>{plan.name}</h2>
           <p>{plan.subtitle} </p>
-          <h3 style={{ marginTop: 0 }}>${plan.price}<span style={{ fontSize: '14px' }}> /store per month</span></h3>
+          <Box style={{display: 'flex', flexDirection: 'column'}}>
+          <h3 style={{ marginTop: 0 }}>${plan.price} </h3>
+          <p><span style={{ fontSize: '14px' }}> /store per month</span></p>
+          </Box>
           <ul style={{ textAlign: 'left', fontSize: '14px', paddingLeft: '20px' }}>
             {plan.features.map((feature, index) => (
               <li key={index}> <CheckCircle sx={{color: 'text.otherSecondary'}} /> {feature}</li>
@@ -110,6 +128,36 @@ const Pricing = () => {
         </div>
       ))}
     </div>
+    <Box sx={{mt:'100px',display: "flex",justifyContent: "center",alignItems: "center", flexDirection: 'column'}}>
+    <Typography sx={{fontSize: "50px"}}>Additional features</Typography>
+    <Stack direction={{xs: 'column', lg: 'row'}} gap={10}>
+      <Box sx={{width: '27%', display: 'flex', flexDirection: 'column', gap: 1}}>
+        <Box>
+          <img src={customBrand} />
+        <Typography variant="h5">Custom Branded App</Typography>
+        <Typography>Dedicated Branded iOS and Android apps on the app stores with your branding applied</Typography>
+        </Box>
+      </Box>
+      <Box sx={{width: '27%', display: 'flex', flexDirection: 'column', gap: 1}}>
+        <Box>
+        <img src={reviewmodule} />
+         <Typography variant="h5">Enterprise</Typography>
+         <Typography>Dedicated Branded iOS and Android apps on the app stores with your branding applied</Typography>
+         </Box>
+      </Box>
+      <Box sx={{width: '27%', display: 'flex', flexDirection: 'column', gap: 1}}>
+        <Box>
+          <img src={plantmodule} />
+         <Typography variant="h5">Plant Mode</Typography>
+         <Typography>Manage orders from multiple stores in your central cleaning facility</Typography>
+         </Box>
+      </Box>
+    </Stack>
+    </Box>
+    <Box sx={{alignSelf: 'center', alignItems: 'center', justifyContent: 'center'}}>
+      <Typography>Frequently asked Questions</Typography>
+      <Typography variant="h3">FAQS</Typography>
+    </Box>
     </>
     )
 }
